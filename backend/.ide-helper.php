7ec9a586eb5809d3b776431c0aa13326493f9b39
Sajeevan <sajeesajeevan1998@gmail.com>
2025-07-09 20:40:09 +0530

    Updates

---
+++ backend/.ide-helper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Illuminate\Foundation {
+    use Illuminate\Foundation\Configuration\Middleware;
+    use Illuminate\Foundation\Configuration\Exceptions;
+
+    /**
+     * @method static \Illuminate\Foundation\Application withRouteMiddleware(callable $callback)
+     * @method static \Illuminate\Foundation\Application withMiddleware(callable $callback)
+     * @method static \Illuminate\Foundation\Application withRouting(array|string $routes)
+     * @method static \Illuminate\Foundation\Application withExceptions(callable $callback)
+     */
+    class Application {}
+}
